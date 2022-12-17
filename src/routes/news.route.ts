0dfaace9b53649ae98caa2fcc0af19100ddbd979
Heshan <mheshan27@gmail.com>
2022-12-17 15:08:56 +0530

    add test

--- src/routes/news.route.ts
+++ src/routes/news.route.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { GetNews, GetNewsByDynamic } from "../contollers/news.controller";
+import { runInContext } from "vm";
 const router = express.Router();
 
 router.route("/").get(GetNews);
 router.route("/:source/data/:page").get(GetNewsByDynamic);
-
+router.route("/test").get((req, res) => {
+  res.send("test working");
+});
 export default router;
